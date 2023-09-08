06f084b4b51a195b324c7646ac80120efa9a7f37
Rashid Abbas <muhammad.rashid@spsnet.com>
2023-09-08 08:45:00 +0500

    changes

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -87,7 +87,13 @@
 	<link href="{{asset('public/frontend/css/jquery-ui.min.css')}}" rel="stylesheet">
 	<link href="{{asset('public/frontend/css/slick.css')}}" rel="stylesheet">
 	<link href="{{asset('public/frontend/css/jquery.gritter.min.css')}}" rel="stylesheet">
-
+	<script type="text/javascript">
+		(function(c,l,a,r,i,t,y){
+			c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+			t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+			y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+		})(window, document, "clarity", "script", "ikjueoq97f");
+	</script>
 	<style type="text/css">
 	:root {
 	  --theme-color: {{ $gtext['theme_color'] }};
